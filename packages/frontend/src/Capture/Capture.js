9a3ba3b1deda0b6515f190152b7d24d40de4d8ff
Daniel Prada <DanielPrada2012@gmail.com>
2023-09-30 14:37:16 -0400

    simple capture setup

--- packages/frontend/src/Capture/Capture.js
+++ packages/frontend/src/Capture/Capture.js
@@ -1,9 +1,64 @@
+import React from 'react';
+import Camera from 'react-html5-camera-photo';
+import 'react-html5-camera-photo/build/css/index.css';
+import ImageUploading from 'react-images-uploading';
+
 function Capture() {
+  const handleTakePhoto = function (dataUri) {
+    console.log(dataUri)
+  }
+
+  const [images, setImages] = React.useState([]);
+  const maxNumber = 69;
+
+  const onChange = (imageList, addUpdateIndex) => {
+    console.log(imageList, addUpdateIndex);
+    setImages(imageList);
+  };
+
+
   return (
     <div className="Capture">
-      <header className="Capture-header">
-        Capture
-      </header>
+      <Camera onTakePhoto = { (dataUri) => { handleTakePhoto(dataUri); } } />
+      <ImageUploading
+        multiple
+        value={images}
+        onChange={onChange}
+        maxNumber={maxNumber}
+        dataURLKey="data_url"
+      >
+        {({
+          imageList,
+          onImageUpload,
+          onImageRemoveAll,
+          onImageUpdate,
+          onImageRemove,
+          isDragging,
+          dragProps,
+        }) => (
+          // write your building UI
+          <div className="upload__image-wrapper">
+            <button
+              style={isDragging ? { color: 'red' } : undefined}
+              onClick={onImageUpload}
+              {...dragProps}
+            >
+              Upload own image
+            </button>
+            &nbsp;
+            <button onClick={onImageRemoveAll}>Remove all images</button>
+            {imageList.map((image, index) => (
+              <div key={index} className="image-item">
+                <img src={image['data_url']} alt="" width="100" />
+                <div className="image-item__btn-wrapper">
+                  <button onClick={() => onImageUpdate(index)}>Update</button>
+                  <button onClick={() => onImageRemove(index)}>Remove</button>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </ImageUploading>
     </div>
   );
 }
