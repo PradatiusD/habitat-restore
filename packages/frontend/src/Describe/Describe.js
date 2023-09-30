c896d9f61807159e4115b3b7b94cdd3e3be21461
Daniel Prada <DanielPrada2012@gmail.com>
2023-09-30 19:31:34 -0400

    Added base description api

--- packages/frontend/src/Describe/Describe.js
+++ packages/frontend/src/Describe/Describe.js
@@ -2,7 +2,8 @@ import TextField from '@mui/material/TextField';
 import Card from '@mui/material/Card';
 import Typography from '@mui/material/Typography';
 import CardContent from '@mui/material/CardContent';
-import MockData from './google-vision-mock-response'
+import Button from '@mui/material/Button';
+// import MockData from './google-vision-mock-response'
 import './Describe.css';
 import {useEffect, useState} from "react";
 function fetchStatus () {
@@ -38,6 +39,7 @@ function Describe() {
   const [imageUrl, setImageUrl] = useState('')
   const [isLoading, setIsLoading] = useState(true)
   const [googleVisionResponse, setGoogleVisionResponse] = useState(null)
+  const [product, setProduct] = useState(null)
 
   useEffect(function () {
     pollUntilValidStatus().then(function (response) {
@@ -60,28 +62,53 @@ function Describe() {
       {
         imageUrl && <img className="user-image" src={imageUrl}/>
       }
-      <div>
-        <TextField label="Product Title" variant="outlined" />
-      </div>
-      <div>
-        <TextField label="Product Description" variant="outlined" multiline rows={3} />
-      </div>
       {
-        googleVisionResponse && (
-          googleVisionResponse.visual_matches.map(function (data) {
-            return (
-              <Card key={data.position} className="card-result">
-                <CardContent>
-                  <img src={data.thumbnail} />
-                  <Typography>{data.title}</Typography>
-                  <a href={data.link}>{data.source}</a>
-                  <img src={data.source_icon} className="image-source-icon" />
-                </CardContent>
-              </Card>
-            )
-          })
+        product && (
+          <div>
+            <TextField
+              value={product && product.title ? product.title :''}
+              variant="outlined"
+              multiline
+              rows={3}
+              fullWidth
+              onChange={(e) => {
+                const copyProduct = JSON.parse(JSON.stringify(product))
+                copyProduct.title = e.target.value
+                setProduct(product)
+              }}
+            />
+            <Button variant="outlined" onClick={() => {}}>Create</Button>
+          </div>
         )
       }
+      <div className="visual-matches-container">
+        {
+          googleVisionResponse && (
+            googleVisionResponse.visual_matches.map(function (data) {
+              return (
+                <Card key={data.position} className="card-result">
+                  <CardContent>
+                    <img className="product-thumbnail" src={data.thumbnail} />
+                    <Typography>{data.title}</Typography>
+                    {
+                      data.price && (
+                        <Typography variant="h6">{data.price.value}</Typography>
+                      )
+                    }
+                    <img src={data.source_icon} className="image-source-icon" />
+                    <a href={data.link} target="_blank">{data.source}</a>
+                    <div className="select-container">
+                      <Button variant="outlined" onClick={() => {
+                        setProduct(data)
+                      }}>Select</Button>
+                    </div>
+                  </CardContent>
+                </Card>
+              )
+            })
+          )
+        }
+      </div>
     </section>
   );
 }
