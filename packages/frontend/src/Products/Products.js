28727cd870c476c3f1fd64365e8d02f469a2d36b
Daniel Prada <DanielPrada2012@gmail.com>
2023-09-30 14:21:39 -0400

    Just some base pages to build out ui

---
+++ packages/frontend/src/Products/Products.js
@@ -0,0 +1,11 @@
+function Purchase() {
+  return (
+    <div className="Purchase">
+      <header className="Purchase-header">
+        Purchase
+      </header>
+    </div>
+  );
+}
+
+export default Purchase;
